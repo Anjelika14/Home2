baca27cdfeee07b46c4720b17d32faba42122b3e
anjelika A <vbfbbf@fg.com>
2024-10-22 15:12:40 +0300

    main

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,19 @@
 public class Main {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
 
-        int score =100;
+        int score = 100;
         int deposit = 1500;
         int bonus;
 
         if (deposit > 1000) {
             bonus = deposit / 100;
-        }
-        else {
+        } else {
             bonus = 0;
 
         }
         int balance = score + deposit + bonus;
 
-        System.out.println ("Ваш баланс:"+balance+"руб. Бонус равен" + bonus+ "руб.");
+        System.out.println("Ваш баланс:" + balance + "руб. Бонус равен" + bonus + "руб.");
 
 
     }
